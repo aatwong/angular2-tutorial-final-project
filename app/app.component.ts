5ef8ffda964bb3ac253fc9a94c961dd8c15ffbf9
Andrew Atwong <andrew.atwong@accenture.com>
2016-12-28 11:22:18 -0800

    added basic navbar from bootswatch

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
 import {Component} from 'angular2/core';
+import {NavbarComponent} from './navbar.component';
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>My First Angular 2 App</h1>
+        <navbar></navbar>
+        `,
+    directives: [NavbarComponent]
 })
 export class AppComponent { }
